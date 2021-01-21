f5e79c291eead0f1df3d6fa5eb1b8fbd8cc96439
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-01-21 14:53:34 +0800

    Update

--- prediction_for_lily_price_and_volume/train.py
+++ prediction_for_lily_price_and_volume/train.py
@@ -5,40 +5,53 @@ import torch.nn as nn
 import utils
 from torch.utils.data import Dataset, DataLoader
 from net import model
+import math
+from tqdm import tqdm
 
 path_csv = r'D:\dataset\lilium_price\108\FS443.csv'
 cloumn = '最高價'
 n = 5  # 取前n天的資料作為特徵
-train_end = 20
+train_end = 200
 
 train_df = utils.read_data(path_csv, cloumn, n, train_end)    # shape = (train_end-n)*(n+1)
 
 # 正歸化
 train = np.array(train_df)
-train = (train - np.mean(train)) / np.std(train)
+# train = (train - np.mean(train)) / np.std(train)
 # to tensor
 train = torch.Tensor(train)
-
 trainset = utils.TrainSet(train)
-trainloader = DataLoader(trainset, batch_size=10, shuffle=False)
+
 
 #train
+batch_size = 10
 LR = 0.0001
-EPOCH = 100
-
+num_epochs = 100
 rnn = model.RNN(n)
-optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)  # optimize all cnn parameters
-loss_func = nn.MSELoss()
+# 選擇優化器與損失函數
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LR) 
+criterion = nn.MSELoss()
 
-for step in range(EPOCH):
-    for tx, ty in trainloader:
-        output = rnn(torch.unsqueeze(tx, dim=0))
-        print(output)
-        loss = loss_func(torch.squeeze(output), ty)
-        optimizer.zero_grad()  # clear gradients for this training step
-        loss.backward()  # back propagation, compute gradients
-        optimizer.step()
-        running_loss = loss.item()
-    print(step, running_loss)
+trainloader = DataLoader(trainset, batch_size=10, shuffle=False)
+train_epoch_size = math.ceil(train.shape[0]/batch_size)
+
+for epoch in range(num_epochs):
+    print('running epoch: {} / {}'.format(epoch, num_epochs))
+    with tqdm(total=train_epoch_size) as pbar:
+        for inputs, target in trainloader:
+            output = rnn(torch.unsqueeze(inputs, dim=0))
+            loss = criterion(torch.squeeze(output), target)
+            running_loss = loss.item()
+            optimizer.zero_grad()  # clear gradients for this training step
+            loss.backward()  # back propagation, compute gradients
+            optimizer.step()
+            
+            #更新進度條
+            pbar.set_postfix(
+                    **{
+                        'running_loss': running_loss,
+                    })
+            pbar.update(1)
+
     # if step % 10:
     #     torch.save(rnn, 'rnn.pkl')

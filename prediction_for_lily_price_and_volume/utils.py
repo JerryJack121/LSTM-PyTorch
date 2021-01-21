69af5c48385baf6eb916ef9aa0b9acc8971744ae
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-01-21 14:14:33 +0800

    Update

--- prediction_for_lily_price_and_volume/utils.py
+++ prediction_for_lily_price_and_volume/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from torch.utils.data import Dataset
 
 #將前n天的資料作為訓練特徵，當天的資料作為Label。
 def generate_df_affect_by_n_days(series, n):
@@ -13,20 +14,22 @@ def generate_df_affect_by_n_days(series, n):
 #載入資料集
 def read_data(path_csv, cloumn,n ,train_end):
     df = pd.read_csv(path_csv, encoding='utf-8')
-    df_col = df[cloumn][:50].astype(float)
+    df_col = df[cloumn].astype(float)
     train_series, test_series = df_col[:train_end], df_col[train_end - n:]
     train_df = generate_df_affect_by_n_days(train_series, n)
 
     return train_df
 
-if __name__ == "__main__":
-    path_csv = r'D:\dataset\lilium_price\108\FS443.csv'
-    cloumn = '最高價'
-    n = 5  #取前n天的資料作為特徵
-    train_end = 20
-    train_df = read_data(path_csv, cloumn, n, train_end)    #train_df.shape = (train_end-n)*(n+1)
+class TrainSet(Dataset):
+    def __init__(self, data):
+        self.data, self.label = data[:, :-1].float(), data[:, -1].float()
+
+    def __getitem__(self, index):
+        return self.data[index], self.label[index]
+
+    def __len__(self):
+        return len(self.data)
+    
 
 
-#正歸化
-# train = (train - np.mean(train)) / np.std(train)
 
